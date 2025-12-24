5356febde9c89b6ba885788554e7439c0fa8d310
gem1n1x <livbraden17@gmail.com>
2025-12-23 19:03:01 -0500

    move word count to stats

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-
-def word_count(book_text):
- words = book_text.split()
- num_words = len(words)
- return num_words
-
+from stats import get_num_words, get_chars_dict, chars_dict_sorted_list
+import sys
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
 
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    print("============ BOOKBOT ============")
+    book_text = get_book_text(sys.argv[1])
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
-    num_words = word_count(text)
+    num_words = get_num_words(text)
+    print("Analyzing book found at", sys.argv[1])
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
 
+    chars_dict = get_chars_dict(text)
+    sorted_chars = chars_dict_sorted_list(chars_dict)
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        ch = item["char"]
+        count = item["num"]
+        if not ch.isalpha():
+            continue
+        print(f"{ch}: {count}")
+    print("============= END ===============")
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-
-from stats import word_count
 
 main()
 
